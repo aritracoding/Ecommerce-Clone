ea107dc5d02741905c328734eaebf12fc67be014
aritracoding <d2aritra@gmail.com>
2021-12-15 01:13:07 +0530

    slider added

---
+++ homepage.js
@@ -0,0 +1,26 @@
+var carValue = 1;
+console.log('load')
+uiRender(carValue);
+
+function carLeft() {
+    --carValue;
+    uiRender(carValue);
+}
+function carRight() {
+    ++carValue;
+    uiRender(carValue);
+}
+
+function uiRender(param) {
+    if (param == 1) {
+        var first = document.querySelector('.first');
+        var second = document.querySelector('.second');
+        second.style.display = 'none';
+        first.style.display = 'flex';
+    } else if (param == 2) {
+        var second = document.querySelector('.second');
+        var first = document.querySelector('.first');
+        first.style.display = 'none';
+        second.style.display = 'flex';
+    }
+}
